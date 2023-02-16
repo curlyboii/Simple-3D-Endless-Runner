b9a11ee21ad8de28e0a6dbe1f9ba3fdda74e4905
Vitalii <vetalsofyk@gmail.com>
2023-02-17 01:25:58 +0200

    Game with build
    Game with build, simple score, and play button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,12 +9,13 @@ public class GameManager : MonoBehaviour
     public GameObject obstacle;
     public Transform spawnPoint; // [osition for spawning obstacle
     int score = 0;
-
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameStart();
         
     }
 
@@ -38,10 +40,21 @@ public class GameManager : MonoBehaviour
     
     }
 
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    
+    }
+
     public void GameStart()
     {
+        player.SetActive(true);
+        playButton.SetActive(false);
+
 
         StartCoroutine("SpawnObstacles");
-    
+
+        InvokeRepeating("ScoreUp", 2f, 1f);
     }
 }
